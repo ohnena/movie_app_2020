65a762fb1d08173b290e9881f0cc919571f1ec2a
ohnena <ohnenaohbaby@gmail.com>
2020-03-24 01:48:26 +0900

    #3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ class App extends React.Component {
     this.setState(current => ({ count: current.count + 1 })); //다이렉트로 state에 접근하는건 안된다!
   };
   minus = () => {
-    
     this.setState(current => ({ count: current.count - 1 })); //다이렉트로 state에 접근하는건 안된다!
   };
+  componentDidMount() {
+    console.log("Component rendered");  //컴포넌트 Mount(생성) 마지막단계에 호출..
+  }
+  componentDidUpdate() {
+    console.log("I just updated");      //컴포넌트 Update 마지막단계에 호출..
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");  //컴포넌트 Unmount 마지막단계에 호출..
+  }
   render() {
     return (
       <div>
@@ -20,8 +28,7 @@ class App extends React.Component {
         <button onClick={this.minus}>Minus</button>
       </div>
     );
-  };
-
+  }
 }
 
 export default App;
