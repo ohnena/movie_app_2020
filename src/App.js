315d47f33cf392283a3ac9444cb10e808dd2dc39
ohnena <ohnenaohbaby@gmail.com>
2020-03-25 04:15:34 +0900

    Revert "#6.2 Building the Navigation"
    This reverts commit 8396ad94302ffeff831aebcdbbd48675874d0b7d.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from "react";
-import Home from "./routes/Home";
-import About from "./routes/About"; //as 컴포넌트 이름 alias설정...안되네?
-import { HashRouter, Route } from "react-router-dom";
-import Navigation from "./components/Navigation";
-import "./App.css";
 
 function App() {
-  return (
-    <HashRouter>
-      <Navigation />
-      {/* <Route path="/" exact={true} component={Home} /> */}
-      <Route path="/about" component={About} />
-    </HashRouter>
-  );
+    return <span>something!</span>;
 }
 
 export default App;
