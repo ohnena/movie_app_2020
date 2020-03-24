8396ad94302ffeff831aebcdbbd48675874d0b7d
ohnena <ohnenaohbaby@gmail.com>
2020-03-25 04:07:29 +0900

    #6.2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Home from "./routes/Home";
+import About from "./routes/About"; //as 컴포넌트 이름 alias설정...안되네?
+import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
-    return <span>something!</span>;
+  return (
+    <HashRouter>
+      <Navigation />
+      {/* <Route path="/" exact={true} component={Home} /> */}
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
